f377eab6617b858f1df325c948e49268c0b517d1
Nasir <nasirkhimani@gmail.com>
2023-07-31 12:43:30 -0400

    security and user creagtion

---
+++ src/routes/routes/mod.rs
@@ -0,0 +1,24 @@
+use crate::models::request::patch_request::EditUserRequest;
+use rocket::serde::json::Json;
+
+pub mod delete_user;
+pub mod get_data_user;
+pub mod hello_name;
+pub mod patch_user;
+pub mod refresh_tokens;
+pub mod get_magnet_link;
+
+enum HelloNameError {
+    OnlyLogin(String),
+    NoOnlyLogin(String),
+    ErrorID,
+}
+
+pub enum EditUserRequestError {
+    Ok(Json<EditUserRequest>),
+    NoneEditModel,
+    BadMail,
+    BadLogin,
+    BadFirstName,
+    BadLastName,
+}
